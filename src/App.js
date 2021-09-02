7157b0d7de52b10f70371cea76578f9bdb42580a
IqueMoraes <iquedemoraes@hotmail.com>
2021-09-01 23:02:25 -0300

    feita a funcao e estilizado o site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(1);
+
+  function randomNumber() {
+    setNumber(Math.floor(Math.random() * (100 - 1 + 1)) + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{number}</h1>
+      <button onClick={randomNumber}>Criar novo numero</button>
     </div>
   );
 }
